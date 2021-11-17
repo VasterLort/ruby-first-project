463745fb006f3be57f6d2901794caa7f3eaefa14
VasterLort <mr.vakar2016@gmail.com>
2021-11-17 23:37:16 +0300

    Add login (we are able to use email or username)

--- db/migrate/20211116023058_add_personal_fields_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddPersonalFieldsToUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :login, :string
-    add_column :users, :name, :string
-    add_column :users, :surname, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_16_023058) do
+ActiveRecord::Schema.define(version: 2021_11_17_200035) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,11 +24,12 @@ ActiveRecord::Schema.define(version: 2021_11_16_023058) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "login"
-    t.string "name"
-    t.string "surname"
+    t.string "username"
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
